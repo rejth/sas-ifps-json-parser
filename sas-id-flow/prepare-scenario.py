179da54595213c07524776dc1c596b5504bf588c
rejth <48617824+rejth@users.noreply.github.com>
2020-05-25 17:54:16 +0300

    Add logic for preparing scenarios

--- sas-id-flow/prepare-scenario.py
+++ sas-id-flow/prepare-scenario.py
@@ -1,13 +1,21 @@
 import json
-def execute(JSONGridString):
-    # "Output: scenarioFiredEventsStr"
-    scenarioFiredEventsList = []
-    scenarioList = json.loads(JSONGridString)[1]['data']
+
+def execute(scenarioGridString):
+    "Output: scenarioFiredEventsStr"
+    # Поля алерта по умолчанию
+    alertDefaultFieldList = ["alertingEventId", "scenarioFiredEventId", "scenarioId", "scenarioName", "scenarioDescription", "scenarioOriginCd", "scenarioFiredEntityType","scenarioFiredEntityId", "score", "displayTypeCd", "displayFlg", "recQueueId", "messageTemplateTxt"] # to edit if needed
+    # Валидный список сценариев, где каждый сценарий - это словарь Python
+    scenarioEventsList = []
+    # Получаем НЕвалидный список сценариев
+    scenarioList = json.loads(scenarioGridString)[1]['data']
+    # Создаем валидный список сценариев
     for scenario in range(len(scenarioList)):
-        scenarioEventsDict = {}
-        scenarioKeysList = ["alertingEventId", "scenarioFiredEventId", "scenarioId", "scenarioName", "scenarioDescription", "scenarioOriginCd", "scenarioFiredEntityType","scenarioFiredEntityId", "score", "displayTypeCd", "displayFlg", "recQueueId", "messageTemplateTxt"]
-        for index in range(len(scenarioKeysList)):
-            scenarioEventsDict[scenarioKeysList[index]] = scenarioList[scenario][index]
-        scenarioFiredEventsList.append(scenarioEventsDict)
-    scenarioFiredEventsStr = json.dumps(scenarioFiredEventsList)
-    return scenarioFiredEventsStr
+        eventDict = {}
+        for key in range(len(alertDefaultFieldList)):
+            eventDict[alertDefaultFieldList[key]] = scenarioList[scenario][key]
+        scenarioEventsList.append(eventDict)
+    # Получаем строку со списком сценариев
+    scenarioFiredEventsStr = json.dumps(scenarioEventsList)
+    return print(scenarioFiredEventsStr)
+
+execute('[{"metadata":[{"ALERTINGEVENTID":"string"},{"SCENARIOFIREDEVENTID":"string"},{"SCENARIOID":"string"},{"SCENARIONAME":"string"},{"SCENARIODESCRIPTION":"string"},{"SCENARIOORIGINCD":"string"},{"SCENARIOFIREDENTITYTYPE":"string"},{"SCENARIOFIREDENTITYID":"string"},{"SCORE":"decimal"},{"DISPLAYTYPECD":"string"},{"DISPLAYFLG":"string"},{"RECQUEUEID":"string"},{"MESSAGETEMPLATETXT":"string"}]},{"data":[["194834970003810275","474092461089218005","SAS_21010226","Account-recipient linked with an employee via contacts","Account-recipient linked with an employee via contacts","ID","vi_transaction","T000177842",50,"TEXT","true","queue_21010226","Account-recipient linked with an employee via contacts"],["194834970003810275","022527020301528011","SAS_21010226","Employee account","Employee account","ID","vi_transaction","T000177842",40,"TEXT","true","queue_21010226","Employee account"],["194834970003810275","674917014227778633","SAS_21010226","Accumulative account","Accumulative account","ID","vi_transaction","T000177842",70,"TEXT","true","queue_21010226","Accumulative account"]]}]')
