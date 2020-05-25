e80c6d4d0d3731f6422f92419d63cb375b169751
rejth <48617824+rejth@users.noreply.github.com>
2020-05-25 17:54:46 +0300

    Add logic to prepearing enrichments

--- sas-id-flow/prepare-enrichment.py
+++ sas-id-flow/prepare-enrichment.py
@@ -1,13 +1,18 @@
 import json
-def execute (enrichmentGridString):
-    # 'Output: enrichmentFiredEventsStr'
-    enrichmentFiredEventsList = []
-    enrichmentDataList = json.loads(enrichmentGridString)[1]['data'][0]
+
+def execute(enrichmentGridString):
+    'Output: enrichmentFiredEventsStr'
+    eventDict = {}
+    # Получаем список полей enrichments
     enrichmentFieldList = json.loads(enrichmentGridString)[0]['metadata']
-    enrichmentEventsDict = {}
-    for index in range(len(enrichmentFieldList)):
-        key = list(enrichmentFieldList[index].keys())
-        enrichmentEventsDict[key[0]] = enrichmentDataList[index]
-    enrichmentFiredEventsList.append(enrichmentEventsDict)
-    enrichmentFiredEventsStr = json.dumps(enrichmentFiredEventsList)
-    return enrichmentFiredEventsStr
+    # Получаем данные полей enrichments
+    enrichmentDataList = json.loads(enrichmentGridString)[1]['data'][0]
+    # Создаем валидный словарь Python с полями и данными enrichments
+    for field in range(len(enrichmentFieldList)):
+        key = list(enrichmentFieldList[field].keys())
+        eventDict[key[0]] = enrichmentDataList[field]
+    # Получаем строку с полями и данными enrichments
+    enrichmentFiredEventsStr = json.dumps(eventDict)
+    return print(enrichmentFiredEventsStr)
+
+execute('[{"metadata":[{"ALERTINGEVENTID":"string"},{"REGION":"string"}]},{"data":[["194834970003810275","Moscow"]]}]')
